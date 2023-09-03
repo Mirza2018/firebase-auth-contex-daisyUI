f9d352e0104fc8f7792eeb1748ced8d3cbda6578
unknown <mirza.eee.4th@gmail.com>
2023-09-03 12:06:39 +0600

    useEffect & onAuthChange &SignOut

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthContexts';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext)
+    console.log(user);
+    const handleLogOut = () => {
+        logOut()
+            .then(result=>{
+                console.log(result.user);
+            })
+            .chatch(error=>{
+                console.log(error);
+            })
+    }
+
     return (
         <div>
             <div className="navbar bg-primary text-primary-content flex justify-around">
@@ -10,6 +23,18 @@ const Header = () => {
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+                {
+                    user ? <div>
+                        <span>{user.email}</span>
+                        <button onClick={handleLogOut}>Sign Out</button>
+                    </div>
+                        :
+                        <>
+                            <Link to='/login'><h4>Login</h4></Link>
+                        </>
+
+
+                }
 
             </div>
         </div>
--- src/providers/AuthContexts.jsx
+++ src/providers/AuthContexts.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from '../firebase/firebase.init';
 
 
@@ -15,11 +15,25 @@ const AuthContexts = ({ children }) => {
     const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
+    const logOut=()=>
+    {
+     return signOut(auth)
+    }
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('User is ',currentUser); 
+            setUser(currentUser)
+        });
+        return () => {
+            unSubscribe
+        }
+        
+    }, [])
 
     const value = { displayName: 'kolmi Lota' }
     const lu = { mama: '10' }
     const values = {
-        user, value, lu,createUser,signIn
+        user, value, lu, createUser, signIn,logOut
     }
 
     return (
